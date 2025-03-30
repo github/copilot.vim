4db702fb2fc20eef8f8f020b1354e7351d7fa882
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2025-03-30 11:00:29 -0700

    chore: use vim.lsp.start instead of vim.lsp.start_client
    Summary: vim.lsp.start_client is deprecated in favor of vim.lsp.start. This
    commit updates the code to use the new function.

--- lua/_copilot.lua
+++ lua/_copilot.lua
@@ -28,7 +28,7 @@ copilot.lsp_start_client = function(cmd, handler_names, opts, settings)
   if #workspace_folders == 0 then
     workspace_folders = nil
   end
-  id = vim.lsp.start_client({
+  id = vim.lsp.start({
     cmd = cmd,
     cmd_cwd = vim.call('copilot#job#Cwd'),
     name = 'GitHub Copilot',
